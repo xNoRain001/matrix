6652893112e14e238279ab9b374df5b7b0977d0c
xNoRain001 <3385842328@qq.com>
2025-08-04 18:37:55 +0800

    update: error message

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -5,8 +5,7 @@ const prefix = '/user'
 export const updateProfile = async userInfo =>
   HTTP.request(`${prefix}/update-profile`, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    data: JSON.stringify(userInfo)
+    data: userInfo
   })
 
 export const updatePassword = async encryptedUserInfo =>
